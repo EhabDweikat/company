bd3e9e4da65127db81c30a8b0fb7a7e99bb1f140
Your Name <ede3377@gmail.com>
2023-03-09 19:20:06 +0200

    second comment

--- src/components/auth/auth.api.js
+++ src/components/auth/auth.api.js
@@ -1,7 +1,13 @@
+
+
 const router=require('express').Router();
 const registerController=require('./controller/registarion.js')
 router.post('/signup',registerController.signup);
 router.get('/confirmEmail/:token',registerController.confirmEmail)
 router.get('/login',registerController.login)
+//update password and reset
+router.post('/forgetPassword', registerController.forgetPassword); // new route for forgetting password
+router.post('/resetPassword/:token', registerController.resetPassword); // new route for resetting password
+
 
 module.exports=router;
--- src/components/auth/controller/registarion.js
+++ src/components/auth/controller/registarion.js
@@ -8,14 +8,14 @@ const signup=async(req,res)=>{
     try{
 
     
-    const{name,email,password}=req.body
+    const{name,email,password,age,country}=req.body
     const user=await userModel.findOne({email}).select('email');
     if (user){
         res.status(409).json({message:'Email is already exist'});
 
     }else{
         bcrypt.hash(password,parseInt(process.env.SALTROUND) , async function(err, hash) {
-            const newuser= new userModel({name,email,password:hash});
+            const newuser= new userModel({name,email,password:hash,age,country});
 
             const token = jwt.sign({ id: newuser._id },process.env.emailToken, { expiresIn: '1h' });
 
@@ -71,8 +71,19 @@ const login=async(req,res)=>{
             if(!match){
                 res.status(400).json({message:'invalid password'})
             }else{
-                const token =jwt.sign({id:user._id},process.env.tokenSignature,{expiresIn:60 * 60 * 24});
+                if(!user.verified){
+                    res.status(400).json({message:'plz confirm your email'});
+
+                }else{
+                    if(!user.isActive){
+                        res.status(400).json({message:'your account is blocked'});
+
+                    }else{
+                        const token =jwt.sign({id:user._id},process.env.tokenSignature,{expiresIn:60 * 60 * 24});
                 res.status(200).json({message:'sucsess',token});
+                    }
+                }
+              
             }
         }
 
@@ -82,4 +93,60 @@ const login=async(req,res)=>{
     }
 }
 
-module.exports={signup,confirmEmail,login};
+
+const forgetPassword = async (req, res) => {
+    try {
+      const { email } = req.body;
+      const user = await userModel.findOne({ email }).select("email");
+  
+      if (!user) {
+        res.status(404).json({ message: "Email not found" });
+        return;
+      }
+  
+      const token = jwt.sign({ id: user._id }, process.env.emailToken, {
+        expiresIn: "1h",
+      });
+  
+      const link = `${req.protocol}://${req.headers.host}${process.env.BASEURL}/auth/resetPassword/${token}`;
+      const message = `<a href ='${link}'>reset your password <\a>`;
+  
+      const info = await sendEmail(email, "Reset Password", message);
+      if (info.accepted?.length > 0) {
+        res.status(200).json({ message: "Reset password link sent to email" });
+      } else {
+        res.status(500).json({ message: "Error sending email" });
+      }
+    } catch (error) {
+      res.status(500).json({ message: "catch error", error });
+    }
+  };
+  
+  const resetPassword = async (req, res) => {
+    try {
+      const { token } = req.params;
+      const { password } = req.body;
+  
+      const decoded = jwt.verify(token, process.env.emailToken);
+      if (!decoded.id) {
+        res.status(400).json({ message: "Invalid payload" });
+        return;
+      }
+  
+      const user = await userModel.findOne({ _id: decoded.id }).select("password");
+      if (!user) {
+        res.status(404).json({ message: "User not found" });
+        return;
+      }
+  
+      bcrypt.hash(password, parseInt(process.env.SALTROUND), async function (err, hash) {
+        user.password = hash;
+        await user.save();
+        res.status(200).json({ message: "Password reset successfully" });
+      });
+    } catch (error) {
+      res.status(500).json({ message: "catch error", error });
+    }
+  };
+  
+  module.exports = { signup, confirmEmail, login, forgetPassword, resetPassword };
