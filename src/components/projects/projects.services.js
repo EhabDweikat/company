b8e30cb1e8e8ea4bbd1b7eeffd2c5be2f3db235d
Your Name <ede3377@gmail.com>
2023-04-03 23:43:32 +0300

    addprogectNew

--- src/components/projects/projects.services.js
+++ src/components/projects/projects.services.js
@@ -39,34 +39,35 @@ const upload = multer({
 }).single('media');
 
 // Function to create a new project
-module.exports.creatNewproject=async(req, res)=> {
-    upload(req, res, function (err) {
-        if (err instanceof multer.MulterError) {
-            // A Multer error occurred when uploading
-            return res.status(500).json({ message: err.message });
-        } else if (err) {
-            // An unknown error occurred when uploading
-            return res.status(500).json({ message: err });
+module.exports.creatNewproject = async (req, res) => {
+    upload(req, res, async function (err) {
+      if (err instanceof multer.MulterError) {
+        return res.status(500).json({ message: err.message });
+      } else if (err) {
+        return res.status(500).json({ message: err });
+      }
+  
+      try {
+        const existingProject = await CategoryModel.findOne({ name: req.body.name });
+        if (existingProject) {
+          return res.status(400).json({ message: "Project with the same name already exists", project: existingProject });
         }
-
-        // Everything went fine, save the new project
+  
         const newProject = new CategoryModel({
-            title: req.body.title,
-            description: req.body.description,
-            status: req.body.status,
-            media: req.file.filename, // Save the filename of the uploaded image
-            //createdAt: new Date(),
-            //updatedAt: new Date()
+            name: req.body.name,
+          description: req.body.description,
+          status: req.body.status,
+          media: req.file.filename,
         });
-
-        newProject.save()
-            
-            .catch((err) => {
-                res.status(500).json({ message: err.message });
-            });
+  
+        const savedProject = await newProject.save();
+        return res.status(201).json({ message: "Project created successfully", project: savedProject });
+      } catch (err) {
+        return res.status(500).json({ message: err.message });
+      }
     });
-}
-
+  };
+  
 module.exports.getAllProjects=async(req,res)=>{
     let categories=await CategoryModel.find({});
     res.status(200).json(categories);
